c96bc6059f79aecd8f3f325be6aee0ab31c9e555
llamato <tguessbacher@gmx.de>
2026-01-23 13:58:46 +0100

    switched smbmount to 5gbit/s nic

--- scripts/wannabeonyx/smbmount.sh
+++ scripts/wannabeonyx/smbmount.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo mount -t cifs //192.168.3.11/raid /home/tina/umounts/smb -o username=tina,vers=3.1.1,multichannel,uid=1000,gid=1000,file_mode=0644,dir_mode=0755
+sudo mount -t cifs //10.20.30.4/raid /home/tina/umounts/smb -o username=tina,vers=3.1.1,multichannel,uid=1000,gid=1000,file_mode=0644,dir_mode=0755
