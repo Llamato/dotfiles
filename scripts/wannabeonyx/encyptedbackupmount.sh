e5ad5b4b7b7f23a65cd036409f57480160ca1351
llamato <tguessbacher@gmx.de>
2026-02-17 19:43:54 +0100

    switched mounts to standard speed network

--- scripts/wannabeonyx/encyptedbackupmount.sh
+++ scripts/wannabeonyx/encyptedbackupmount.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-sudo bcachefs unlock -k session /dev/sdc1
-sudo bcachefs mount /dev/sdc1 /home/tina/umounts/bdata
+mkdir -p /home/tina/umounts/bdata
+sudo bcachefs unlock -k session /dev/sdd1
+sudo bcachefs mount /dev/sdd1 /home/tina/umounts/bdata
