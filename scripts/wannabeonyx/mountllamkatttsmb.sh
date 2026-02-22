00cfcb5839d4809c1a4f60a24b2590186033b369
llamato <tguessbacher@gmx.de>
2026-02-22 14:57:34 +0100

    Added more mounting scripts

---
+++ scripts/wannabeonyx/mountllamkatttsmb.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo mount -t cifs //10.20.30.4/raid /home/tina/umounts/llamkatttsmb -o username=tina,vers=3.1.1,multichannel,uid=1000,gid=1000,file_mode=0644,dir_mode=0755
---
+++ scripts/wannabeonyx/mountwannabeinthebasementnfs.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo mount -t nfs -o nolock 10.20.30.3:/mnt ~/umounts/nixnasnfs/
