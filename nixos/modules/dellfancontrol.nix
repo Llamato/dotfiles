d3083c1ef15edb320c9d9d5ebeb5bdc827fbdf35
llamato <tguessbacher@gmx.de>
2026-02-23 20:20:15 +0100

    Revert "moved dellfancontroller to separate bash script"
    This reverts commit c11679fe1c04434abdbeeffd4dd38c781f5b20de.

--- nixos/modules/dellfancontrol.nix
+++ nixos/modules/dellfancontrol.nix
@@ -8,7 +8,36 @@
 let
   ipmitool = pkgs.ipmitool;
 
-  dellfancontroller = pkgs.writeShellScriptBin "dellfancontroller" (lib.readFile ../../scripts/wannabeinthebasement/dellfancontroller.sh);
+  dellfancontroller = pkgs.writeShellScriptBin "dellfancontroller" ''
+    #!/bin/sh
+    LOWTEMP=50
+    MIDTEMP=60
+    HIGHTEMP=80
+
+    LOWSPEED=0x06
+    MIDSPEED=0x20
+    HIGHSPEED=0x64
+
+    ${ipmitool}/bin/ipmitool raw 0x30 0x30 0x01 0x00
+    while true; do
+        HIGHESTTEMP=$(${ipmitool}/bin/ipmitool sdr type temperature | ${pkgs.gnugrep}/bin/grep -Po '\d+(?= degrees C)' | sort -n | tail -1)
+        if [ -z "$HIGHESTTEMP" ]; then
+            echo "No temperature data found"
+            sleep 15
+            continue
+        fi
+        
+        if [ $HIGHESTTEMP -ge $HIGHTEMP ]; then
+            ${ipmitool}/bin/ipmitool raw 0x30 0x30 0x02 0xff $HIGHSPEED
+        elif [ $HIGHESTTEMP -ge $MIDTEMP ]; then
+            ${ipmitool}/bin/ipmitool raw 0x30 0x30 0x02 0xff $MIDSPEED
+        elif [ $HIGHESTTEMP -le $LOWTEMP ]; then
+            ${ipmitool}/bin/ipmitool raw 0x30 0x30 0x02 0xff $LOWSPEED
+        fi
+        
+        sleep 15
+    done
+  '';
 in
 {
   options.services.dellfancontroller = {
