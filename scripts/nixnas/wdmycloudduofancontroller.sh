82c4e1e3e3bb6620cdb0391e2dab4611501b5874
llamato <tguessbacher@gmx.de>
2026-02-24 12:54:52 +0100

    Removed unused code

--- scripts/nixnas/wdmycloudduofancontroller.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-while true; do
-    CPU_IDLE=$(mpstat | tail -n 1 | rev | cut -f1 -d" " | rev | cut -f1 -d.)
-    CPU_USAGE=$((100-$CPU_IDLE))
-    echo $CPU_USAGE > /sys/devices/platform/980070d0.pwm/dutyRate3
-    sleep 15
-done
