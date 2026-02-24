d00dc4c54912fe54f4f7b3af272d94c49b83b857
llamato <tguessbacher@gmx.de>
2026-02-24 15:07:11 +0100

    switched encrypteddatamount to use uuids

--- scripts/wannabeonyx/encypteddatamount.sh
+++ scripts/wannabeonyx/encypteddatamount.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir -p /home/tina/umounts/data
-sudo bcachefs unlock -k session /dev/sdi1
-sudo bcachefs mount /dev/sdi1 /home/tina/umounts/data
+sudo bcachefs unlock -k session /dev/disk/by-uuid/9bae3504-266e-46ba-8dc7-d33858eae1c5
+sudo bcachefs mount /dev/disk/by-uuid/9bae3504-266e-46ba-8dc7-d33858eae1c5 /home/tina/umounts/data
