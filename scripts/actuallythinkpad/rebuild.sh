e67fea5621c6fec32f7345935110f3a16fa69772
llamato <tguessbacher@gmail.com>
2026-05-06 20:45:25 +0200

    added rebuild script for wannabethinkpad

--- scripts/actuallythinkpad/rebuild.sh
+++ scripts/actuallythinkpad/rebuild.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+<<<<<<< HEAD
 sudo nixos-rebuild switch --print-build-logs --flake .#actuallythinkpad
+=======
+sudo nixos-rebuild switch --print-build-logs --flake .#actuallythinkpad
+>>>>>>> f2ee7a0 (added rebuild script for wannabethinkpad)
