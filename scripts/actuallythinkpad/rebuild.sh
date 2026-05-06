a2ba31eddb51814cbb2d4bf410406cb98e4ec6d3
llamato <tguessbacher@gmx.de>
2026-05-06 20:56:44 +0200

    fixed up merge

--- scripts/actuallythinkpad/rebuild.sh
+++ scripts/actuallythinkpad/rebuild.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-<<<<<<< HEAD
 sudo nixos-rebuild switch --print-build-logs --flake .#actuallythinkpad
-=======
-sudo nixos-rebuild switch --print-build-logs --flake .#actuallythinkpad
->>>>>>> f2ee7a0 (added rebuild script for wannabethinkpad)
