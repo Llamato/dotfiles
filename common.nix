6f7a93f6bc393a2d94548aedbd4ed28296e02909
llamato <tguessbacher@gmx.de>
2026-06-02 00:30:51 +0200

    updated common.nix

--- common.nix
+++ common.nix
@@ -1,10 +1,10 @@
 { config, ... }: {
   nix = {
     #package = pkgs.lixPackageSets.stable.lix;
+    optimise.automatic = true;
     settings = {
       log-lines = "35";
       max-jobs = "auto";
-      auto-optimise-store = true;
       nix-path = config.nix.nixPath;
       experimental-features = "nix-command flakes";
       #download-buffer-size = 524288000; # 512 MB
