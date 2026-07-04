cc0cea4ffaf00f40e5b4163ace02aaa354de4c57
llamato <tguessbacher@gmx.de>
2026-07-04 02:42:44 +0200

    removed steam from wannabethinkpad

--- nixos/hosts/wannabethinkpad.nix
+++ nixos/hosts/wannabethinkpad.nix
@@ -102,7 +102,7 @@
     #steam-run
   ];
 
-  programs.steam.enable = true;
+  #programs.steam.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
