b7a641998aef7eccafbc4c5be146e6925cb2051a
llamato <tguessbacher@gmx.de>
2026-07-30 18:37:57 +0200

    changed encrypted backup mount to uuid

--- scripts/wannabeonyx/encyptedbackupmount.sh
+++ scripts/wannabeonyx/encyptedbackupmount.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir -p /home/tina/umounts/bdata
-sudo bcachefs unlock -k session /dev/sdl1
-sudo bcachefs mount /dev/sdl1 /home/tina/umounts/bdata
+sudo bcachefs unlock -k session /dev/disk/by-uuid/1bc5c371-9d22-4c59-a835-b6e79862a935
+sudo bcachefs mount /dev/disk/by-uuid/1bc5c371-9d22-4c59-a835-b6e79862a935 /home/tina/umounts/bdata
