8cafce42b575c63a81e41bbc82837e9edc378875
llamato <tguessbacher@gmx.de>
2026-08-13 15:21:23 +0200

    Added psid package

--- nixos/packages/psid/package.nix
+++ nixos/packages/psid/package.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, fetchFromGitHub, autoreconfHook }:
+{ lib, stdenv, fetchFromGitHub, autoreconfHook, autoconf-archive, automake, libtool, pkg-config }:
 
 stdenv.mkDerivation rec {
   pname = "psid64";
@@ -8,10 +8,24 @@ stdenv.mkDerivation rec {
     owner = "hermansr";
     repo = "psid64";
     rev = "v${version}";
-    hash = "sha256-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX=";
+    hash = "sha256-cxtKHH25urL3Y5+l2GNCSJZdgRTstaTZfc0W3d8jEqM=";
   };
 
-  nativeBuildInputs = [ autoreconfHook ];
+  nativeBuildInputs = [
+    autoreconfHook
+    autoconf-archive
+    automake
+    libtool
+    pkg-config
+  ];
+
+  # Ensure aclocal finds local macros
+  ACLOCAL_PATH = "./macros";
+
+  # Patch to remove -Werror
+  prePatch = ''
+    sed -i "s/-Werror//g" configure.ac
+  '';
 
   meta = with lib; {
     description = "PSID file converter and native Commodore 64 music player";
