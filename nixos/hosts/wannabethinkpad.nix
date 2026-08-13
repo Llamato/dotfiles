1617b05249f08975ca8bd998507883aa5242c004
llamato <tguessbacher@gmx.de>
2026-08-13 16:46:40 +0200

    disabled openssh server on wannabethinkpad

--- nixos/hosts/wannabethinkpad.nix
+++ nixos/hosts/wannabethinkpad.nix
@@ -93,13 +93,13 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  #services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  networking.firewall.enable = false;
+  networking.firewall.enable = true;
 
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
