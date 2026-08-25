7b4145edd9a07cf2e46e19ee0fd9a81f8912dc44
llamato <tguessbacher@gmx.de>
2026-08-25 04:32:03 +0200

    updated lto integrations

---
+++ nixos/packages/stenc/package.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  autoreconfHook,
+  pkg-config,
+  pandoc,
+}:
+
+stdenv.mkDerivation {
+  pname = "stenc";
+  version = "2.0.1";
+  src = fetchFromGitHub {
+    owner = "scsitape";
+    repo = "stenc";
+    rev = "2.0.1";
+    hash = "sha256-M7b+T0mm2QTP1LqqjdKV/NWZ60DrueFEnN1unwCOeH4=";
+  };
+
+  nativeBuildInputs = [
+    autoreconfHook
+    pandoc
+    pkg-config
+  ];
+
+  meta = with lib; {
+    description = "SCSI Tape Encryption Manager";
+    homepage = "https://github.com/scsitape/stenc";
+    license = licenses.gpl2;
+    platforms = platforms.unix;
+    maintainers = with maintainers; [ llamato ];
+  };
+}
