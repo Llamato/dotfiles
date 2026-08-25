90c841cd1ef84ed549ff3dd3937e2af982c54dfe
llamato <tguessbacher@gmx.de>
2026-08-25 13:37:49 +0200

    Added Tek4010 package

---
+++ nixos/packages/tek4010/package.nix
@@ -0,0 +1,47 @@
+{
+  stdenv,
+  lib,
+  fetchFromGitHub,
+  gcc,
+  gnumake,
+  gtk3,
+  gtk3-x11,
+  cairo,
+  pkg-config
+}:
+stdenv.mkDerivation {
+  pname = "tek4010";
+  version = "1.9.0";
+  src = fetchFromGitHub {
+    owner = "Llamato";
+    repo = "Tek4010";
+    rev = "master";
+    hash = "sha256-5wUEIdiZSbC2yq0pCU2lVdwaa+QEADIZOqEGYa7mH4c=";
+  };
+
+  nativeBuildInputs = [ 
+    gcc
+    gnumake
+  ];
+
+  buildInputs = [
+    gtk3
+    gtk3-x11
+    cairo.dev
+    pkg-config
+  ];
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/bin
+    cp tek4010 $out/bin
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "Tek4010 - Tektronics 4010 emulator";
+    license = licenses.gpl3;
+    maintainers = [ maintainers.llamato ];
+    platforms = platforms.unix;
+  };
+}
