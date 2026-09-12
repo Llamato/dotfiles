740bc825d335199193ac1a9168ce2f6c191b1b92
llamato <tguessbacher@gmx.de>
2026-09-13 01:44:34 +0200

    added root to rusted users

--- common.nix
+++ common.nix
@@ -4,16 +4,16 @@
     optimise.automatic = true;
     settings = {
       trusted-substituters = [
-        "http://10.20.30.3:3000/"
-        "http://192.168.3.14:3000/"
-        "https://homelab.lllamato.dev:3000"
+        "http://10.20.30.3:5000/"
+        "http://192.168.3.14:5000/"
+        "https://homelab.lllamato.dev:5000"
       ];
       trusted-public-keys = [ 
         "wannabeinthebasement:Gwgwnvx67OKz2htvy/p770qeajfVUrVAlI+K3nxcOJM="
         "192.168.3.14:208dtjaunWYQX5P2w3tQVWepqqHsRP8mM6cExexQHBo="
         "homelab.llamato.dev:6znaEdEvZ1P5GQXD1BZ5KUY0uP56/t2iu/dMj0fJJg8="
       ];
-      trusted-users = [ "tina" ];
+      trusted-users = [ "root" "tina" ];
       system-features = [ 
         "gccarch-armv7-a"
       ];
