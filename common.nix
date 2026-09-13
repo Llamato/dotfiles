de9c3a4fa98ca4d6e5faed8d84edfe99827f15f6
llamato <tguessbacher@gmx.de>
2026-09-13 02:12:19 +0200

    updated keys

--- common.nix
+++ common.nix
@@ -3,14 +3,14 @@
     #package = pkgs.lixPackageSets.stable.lix;
     optimise.automatic = true;
     settings = {
-      trusted-substituters = [
+      substituters = [
         "http://10.20.30.3:5000/"
         "http://192.168.3.14:5000/"
         "https://homelab.lllamato.dev:5000"
       ];
       trusted-public-keys = [ 
         "wannabeinthebasement:Gwgwnvx67OKz2htvy/p770qeajfVUrVAlI+K3nxcOJM="
-        "192.168.3.14:208dtjaunWYQX5P2w3tQVWepqqHsRP8mM6cExexQHBo="
+        "192.168.3.14-1:WN5/PjgQlzQ+PSDMXjv+B5rXKzkFtRg1+/UkAmnEvkM="
         "homelab.llamato.dev:6znaEdEvZ1P5GQXD1BZ5KUY0uP56/t2iu/dMj0fJJg8="
       ];
       trusted-users = [ "root" "tina" ];
