cde8d91560c01395b918dd18fe3721758ddea7a1
llamato <tguessbacher@gmx.de>
2026-09-18 21:28:43 +0200

    added kurogane to llamkattserver

--- nixos/hosts/llamkattthpmicroserver.nix
+++ nixos/hosts/llamkattthpmicroserver.nix
@@ -2,7 +2,7 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ self, pkgs, ... }: {
+{ self, inputs, pkgs, ... }: {
   
   #Tinas edits
   boot.kernelPackages = pkgs.linuxPackages_latest;
