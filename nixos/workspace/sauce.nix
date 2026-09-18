ba30ffe238d0b338f2cf0ee7eb0b61e3a55c031d
llamato <tguessbacher@gmx.de>
2026-09-18 23:42:09 +0200

    deleted sauce workspace

--- nixos/workspace/sauce.nix
+++
@@ -1,9 +0,0 @@
-{inputs, pkgs, ... }: {
-  environment.systemPackages = [
-    #Tina's flakes
-    inputs.gcalc.packages.${pkgs.system}.default
-    inputs.gcrypt.packages.${pkgs.system}.default
-    inputs.gbounce.packages.${pkgs.system}.default
-    inputs.cbmtext.packages.${pkgs.system}.demo
-  ];
-}
