b4397a8d508536678738a4df76cbb45c2e39d8a5
llamato <tguessbacher@gmx.de>
2026-09-20 00:11:26 +0200

    modernized qbittorrent-nox config

--- nixos/services/qbittorrent-nox.nix
+++ nixos/services/qbittorrent-nox.nix
@@ -1,18 +1,17 @@
 { pkgs, ... }: {
-  environment.systemPackages = with pkgs; [
-    qbittorrent-nox
-  ];
-
-  systemd.services.qbittorrent-nox = {
-    description = "qbittorrent-nox client with webui";
-    wantedBy = [ "multi-user.target" ];
-
-    serviceConfig = {
-      Type = "simple";
-      Restart = "always";
-      RestartSec = "15s";
-      User = "tina";
-      ExecStart = "${pkgs.qbittorrent-nox}/bin/qbittorrent-nox";
-    };
+  services.qbittorrent = {
+    package = pkgs.qbittorrent-nox;
+    openFirewall = true;
+    serverConfig = {
+      LegalNotice.Accepted = true;
+      Preferences = {
+        WebUI = {
+          Username = "tina";
+          Password = "6301";
+        };
+        General.Locale = "en";
+      };
+    }
+    ;
   };
 }
