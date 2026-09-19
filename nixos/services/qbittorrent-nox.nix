0f633e73b53a80e1d4be89e95b0b9e2b149fc909
llamato <tguessbacher@gmx.de>
2026-09-20 00:45:33 +0200

    updated qbittorrent-nox config to allow all connections

--- nixos/services/qbittorrent-nox.nix
+++ nixos/services/qbittorrent-nox.nix
@@ -6,6 +6,8 @@
       LegalNotice.Accepted = true;
       Preferences = {
         WebUI = {
+          Address = "*";
+          AuthSubnetWhitelistEnabled=false;
           Username = "tina";
           Password_PBKDF2="@ByteArray(6a5uZbO9yKW5/ScWabltvw==:xVSh8UHwV0TgnBs1t0aYnARXbVBD8zmGYLpMnFgdChIOmLURFzY8TEh1aBkjUh3P7bl17q0QmyNp5esW5RLMXw==)";
         };
