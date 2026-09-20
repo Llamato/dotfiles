62f404d4c6004e2724299a7d0910c93fa679e2df
llamato <tguessbacher@gmx.de>
2026-09-20 23:43:55 +0200

    Added more cross pkgs

---
+++ nixos/packages/pkgsCross/tek4010/package.nix
@@ -0,0 +1,50 @@
+{
+  stdenv,
+  lib,
+  fetchFromGitHub,
+  gcc,
+  gnumake,
+  gtk3,
+  gtk3-x11,
+  cairo,
+  pkg-config
+}:
+stdenv.mkDerivation {
+  pname = "tek4010";
+  version = "1.9.0";
+  src = fetchFromGitHub {
+    owner = "Llamato";
+    repo = "Tek4010";
+    rev = "master";
+    hash = "sha256-5wUEIdiZSbC2yq0pCU2lVdwaa+QEADIZOqEGYa7mH4c=";
+  };
+
+strictDeps = true;
+__structuredAttrs = true;
+
+  nativeBuildInputs = [ 
+    gcc
+    gnumake
+    pkg-config
+  ];
+
+  buildInputs = [
+    gtk3
+    gtk3-x11
+    cairo.dev
+  ];
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/bin
+    cp tek4010 $out/bin
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "Tek4010 - Tektronics 4010 emulator";
+    license = licenses.gpl3;
+    platforms = platforms.unix;
+   #maintainers = with maintainers; [ llamato ];
+  };
+}
